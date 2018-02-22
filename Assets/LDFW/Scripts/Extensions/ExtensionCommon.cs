d469a573c79457347b2d46bfe6494e1e9d832489
ealton <longjdai@gmail.com>
2018-02-22 21:51:24 +0800

    update

---
+++ Assets/LDFW/Scripts/Extensions/ExtensionCommon.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+
+using System.Collections;
+
+namespace LDFW
+{
+
+    public static partial class CommonExtensions
+    {
+        public static string Concatenate(this string str, params string[] strArray)
+        {
+            if (strArray != null)
+            {
+                foreach (var part in strArray)
+                    str += part;
+            }
+
+            return str;
+        }
+    }
+
+}
